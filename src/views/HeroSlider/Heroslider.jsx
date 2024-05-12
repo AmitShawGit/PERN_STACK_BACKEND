eb189fa0d96274dd0e979620531583706cf50364
amit <amitshaw1421@gmail.com>
2024-05-12 15:51:51 +0530

    post image all ui done

--- src/views/HeroSlider/Heroslider.jsx
+++ src/views/HeroSlider/Heroslider.jsx
@@ -1,27 +1,52 @@
-import React from 'react';
-import { CCard, CCardBody, CRow, CCol } from '@coreui/react';
+import React, { useState, createRef } from 'react';
+import { CCard, CCardBody, CRow, CCol, CButton } from '@coreui/react';
 const Heroslider = () => {
+
+  let [previewImage, setPreviewImage] = useState(null);
+  let [isVisible, setIsVisible] = useState(false)
+  let imageFile = createRef();
+
+  let uploadImage = (e) => {
+    let imgObject = URL.createObjectURL(e.target.files[0])
+    setPreviewImage(imgObject)
+    setIsVisible(true)
+  }
+  let removeImage = () => {
+    setPreviewImage(null);
+    imageFile.current.value = "";
+    setIsVisible(false)
+  }
+  let postImage = () => {
+
+  }
+
   return (
     <>
 
-        <CCard>
-          <CCardBody>
-      <CRow>
+      <CCard>
+        <CCardBody>
+          <CRow>
             <CCol md={5}>
               <label htmlFor="addImg">Add Image</label>
-              <input type="file" name="sliderImg" id="addImg" className='form-control' />
+              <input type="file" name="sliderImg" id="addImg" className='form-control' onChange={uploadImage} ref={imageFile} />
             </CCol>
+            <CCol md={3}>
+              {isVisible && <CButton className="btn btn-success mt-4" onClick={postImage}>Post</CButton>}
+            </CCol>
+          </CRow>
+          <CRow >
+            <CCol md={4} className='preview-slider-img'>
+              {previewImage && (
+                <div>
+                  <span><i className="fa fa-x" onClick={removeImage}></i></span>
+                  <img src={previewImage} alt="" className='img-fluid' />
+                </div>
+              )}
+            </CCol>
+          </CRow>
+        </CCardBody>
 
-      </CRow>
-      <CRow >
-        <CCol md={4} className='preview-slider-img'>
-          <span><i className="fa fa-x"></i></span>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/5/58/Dscn4066-pendennis-dark-in-shed_crop_1200x600.jpg" alt="" className='img-fluid' />
-        </CCol>
-      </CRow>
-          </CCardBody>
-
-        </CCard>
+      </CCard>
 
 
     </>
